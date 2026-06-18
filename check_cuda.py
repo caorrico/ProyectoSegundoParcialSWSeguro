b5eee31bcbeb150b59f6a8748642880cf772319e
Cesar Loor <ciloor2@espe.edu.ec>
2026-06-18 08:16:13 -0500

    refact

--- check_cuda.py
+++
@@ -1,8 +0,0 @@
-import torch
-print("CUDA disponible:", torch.cuda.is_available())
-print("Versión PyTorch:", torch.__version__)
-print("Número de GPUs:", torch.cuda.device_count())
-if torch.cuda.is_available():
-    print("GPU:", torch.cuda.get_device_name(0))
-else:
-    print("GPU: N/A")
